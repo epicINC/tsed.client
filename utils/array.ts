fe21d91a15357a856814befb442ad40859fdca39
JayYU <git@slightboy.net>
2021-09-22 23:31:52 +0800

    temp

--- utils/array.ts
+++ utils/array.ts
@@ -1,6 +1,6 @@
 
 export class ArrayImpl {
-	partition<T>(data: T[], predicate: Predicate<[T, number?]>) {
+	partition<T>(data: T[], predicate: (e: T, i?: number) => boolean) {
 		return data.reduce<[T[], T[]]>((r, e, i) => {
 			r[predicate(e, i) ? 0 : 1].push(e)
 			return r
